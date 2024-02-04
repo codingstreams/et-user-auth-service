e155d189a78deb4585ec6ce035c232d98ddf4de5
Coding Streams <akxhdev@outlook.com>
2024-02-04 22:19:57 +0530

    Initial Commit

---
+++ src/main/java/in/codingstreams/etuserauthservice/EtUserAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package in.codingstreams.etuserauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtUserAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtUserAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/codingstreams/etuserauthservice/EtUserAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.codingstreams.etuserauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtUserAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
